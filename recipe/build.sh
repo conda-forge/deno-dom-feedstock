10a19ea25af4f966c292631a4883fad2a7ce7968
Michael Sarahan <msarahan@gmail.com>
2022-08-03 23:06:38 -0500

    fix variable setting and improve tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,9 +13,9 @@ fi
 cp target/release/libplugin.$ext $PREFIX/lib/deno_dom.$ext
 
 mkdir -p "${PREFIX}/etc/conda/activate.d"
-echo "export DENO_DOM_PLUGIN=$PREFIX/lib/deno_dom.$ext\n" > "${PREFIX}/etc/conda/activate.d/deno_dom.sh"
-echo "export DENO_DOM_VERSION=$PKG_VERSION-alpha\n" >> "${PREFIX}/etc/conda/activate.d/deno_dom.sh"
+echo "export DENO_DOM_PLUGIN=$PREFIX/lib/deno_dom.$ext" > "${PREFIX}/etc/conda/activate.d/deno_dom.sh"
+echo "export DENO_DOM_VERSION=$PKG_VERSION-alpha" >> "${PREFIX}/etc/conda/activate.d/deno_dom.sh"
 
 mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-echo "unset DENO_DOM_PLUGIN\n" > "${PREFIX}/etc/conda/deactivate.d/deno_dom.sh"
-echo "unset DENO_DOM_VERSION\n" >> "${PREFIX}/etc/conda/deactivate.d/deno_dom.sh"
+echo "unset DENO_DOM_PLUGIN" > "${PREFIX}/etc/conda/deactivate.d/deno_dom.sh"
+echo "unset DENO_DOM_VERSION" >> "${PREFIX}/etc/conda/deactivate.d/deno_dom.sh"
