382478b3d519749c8a06dd3fbb3b2303180de86f
Michael Sarahan <msarahan@gmail.com>
2023-03-06 15:04:43 -0600

    build earlier version 0.1.23 for quarto 1.2.335

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 cargo bundle-licenses --format yaml --output DENO_DOM_THIRDPARTY_LICENSES.yml
 cargo build --release
@@ -14,10 +14,12 @@ mkdir -p $PREFIX/lib
 LIB_OUTPUT=$(find target -name libplugin.$ext | tail -n 1)
 cp $LIB_OUTPUT $PREFIX/lib/deno_dom.$ext
 
-mkdir -p "${PREFIX}/etc/conda/activate.d"
-echo "export DENO_DOM_PLUGIN=$PREFIX/lib/deno_dom.$ext" > "${PREFIX}/etc/conda/activate.d/deno_dom.sh"
-echo "export DENO_DOM_VERSION=$PKG_VERSION-alpha" >> "${PREFIX}/etc/conda/activate.d/deno_dom.sh"
+activateFile="${PREFIX}/etc/conda/activate.d/deno_dom.sh"
+deactivateFile="${PREFIX}/etc/conda/deactivate.d/deno_dom.sh"
+mkdir -p $(dirname $activateFile)
+mkdir -p $(dirname $deactivateFile)
+echo "export DENO_DOM_PLUGIN=$PREFIX/lib/deno_dom.$ext" > "${activateFile}"
+echo "export DENO_DOM_VERSION=$PKG_VERSION-alpha" >> "${activateFile}"
 
-mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-echo "unset DENO_DOM_PLUGIN" > "${PREFIX}/etc/conda/deactivate.d/deno_dom.sh"
-echo "unset DENO_DOM_VERSION" >> "${PREFIX}/etc/conda/deactivate.d/deno_dom.sh"
+echo "unset DENO_DOM_PLUGIN" > "${deactivateFile}"
+echo "unset DENO_DOM_VERSION" >> "${deactivateFile}"
